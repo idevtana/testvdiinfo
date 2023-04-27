1e38b4f92b574a197e937b5975f2e1a13ec793bf
Gino Lee <ginoleeandriampaniry@gmail.com>
2023-04-27 19:35:52 +0300

    Add js for project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,21 @@
+
 import '../scss/app.scss';
 
 document.addEventListener('DOMContentLoaded', function () {
+    document.getElementsByClassName('header__menulist--mobileaction')[0].addEventListener('click', function(){
+        console.log(document.getElementsByClassName('header__menulist--mobileaction')[0].classList);
+        document.getElementsByClassName('header__menulist--mobileaction')[0].classList.toggle('active');
+    });
+
+    let $inputMaxRange = document.querySelectorAll('.content__aside-inputrange__maxrange input[type="range"]');
+    for(var i=0;i<=$inputMaxRange.length;i++){
+        var $inputclick = $inputMaxRange[i];
+        $inputclick.addEventListener('click', function(){
+            console.log($inputclick);
+            this.nextElementSibling.children[0].innerHTML = this.value;
+        });
+    }
 });
+
+
 
