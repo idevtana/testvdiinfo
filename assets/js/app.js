749f6f6ae835f2db4f60e32546d3fdbc15cd3e6c
Gino Lee <ginoleeandriampaniry@gmail.com>
2023-04-28 02:22:50 +0300

    Correct range slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,22 +7,54 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementsByClassName('header__menulist--mobileaction')[0].classList.toggle('active');
     });
 
-    function displayRangeValue($classe, $type){
+    function fillColorRange($rangeMinInput, $rangeMaxInput, $track){
+      let $percent1 = $rangeMinInput.value / $rangeMinInput.max * 100;
+      let $percent2 = $rangeMaxInput.value / $rangeMaxInput.max * 100;
+      // if($track) $track.style.background = `linear-gradient(to right, #dadae5 ${$percent1}%, #320F00 ${$percent1}%, #320F00 ${$percent2}%, #dadae5 ${$percent2}%)`;
+      if($track) $track.style.background = `linear-gradient(to right, #320F00 ${$percent1}%, #320F00 ${$percent2}%)`;
       
+    }
+    function displayRangeValue($classe, $type){
       let $inputRange = document.querySelectorAll($classe);
       if($inputRange){
+        var $rangeFind = [];
+        var $minGap = [];
         for(var i=0;i<$inputRange.length;i++){
             let $inputclick = $inputRange[i];
-            $inputclick.addEventListener('change', function(){
-                console.log(this.value);
-                this.nextElementSibling.children[0].innerHTML = this.value;
-                if($type == 'max'){
-                  //bloqué le min
+            
+            $rangeFind[i] = $type == 'max' ? $inputclick.parentElement.previousElementSibling.children[0] : $inputclick.parentElement.nextElementSibling.children[0]; 
+            $minGap[i] = $type == 'max' ? $rangeFind[i].getAttribute('min') : $inputclick.getAttribute('min');
+            
+            fillColorRange(
+              $type == 'max' ? $inputclick.parentElement.previousElementSibling.children[0]:$inputclick, 
+              $type == 'max' ? $inputclick:$inputclick.parentElement.nextElementSibling.children[0],
+              $type == 'max' ? $inputclick.parentElement.previousElementSibling.previousElementSibling.children[0]:$inputclick.parentElement.previousElementSibling, 
+            );
+
+            $inputclick.oninput = (e) => {
+              
+                e.target.nextElementSibling.children[0].innerHTML = e.target.value;
+                fillColorRange(
+                  $type == 'max' ? e.target.parentElement.previousElementSibling.children[0] : e.target, 
+                  $type == 'max' ? e.target : e.target.parentElement.nextElementSibling.children[0],
+                  $type == 'max' ? e.target.parentElement.previousElementSibling.previousElementSibling.children[0] : e.target.parentElement.previousElementSibling, 
+                );
+                let $gap = 0;
+                if(typeof $type != 'undefined'){
+                  if($type == "min"){   
+                    if(parseInt(e.target.parentElement.nextElementSibling.children[0].value) - parseInt(e.target.value) <= $gap ){
+                      e.target.value = parseInt(e.target.parentElement.nextElementSibling.children[0].value) - $gap;
+                    }
+                  }
+                  else if(parseInt(e.target.value) - parseInt(e.target.parentElement.previousElementSibling.children[0].value) <= $gap ){
+                      e.target.value = parseInt(e.target.parentElement.previousElementSibling.children[0].value) + $gap;
+                      
+                  }
+                  
                 }
-                else{ 
-                  //bloqué le max
-                }
-            });
+
+                
+            }
         }
       }
     }
