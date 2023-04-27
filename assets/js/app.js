638dde96a92a68cfd0e124f0fd5f7c90b261d9a4
Gino Lee <ginoleeandriampaniry@gmail.com>
2023-04-27 21:00:59 +0300

    Range slider update js

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,14 +7,31 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementsByClassName('header__menulist--mobileaction')[0].classList.toggle('active');
     });
 
-    let $inputMaxRange = document.querySelectorAll('.content__aside-inputrange__maxrange input[type="range"]');
-    for(var i=0;i<=$inputMaxRange.length;i++){
-        var $inputclick = $inputMaxRange[i];
-        $inputclick.addEventListener('click', function(){
-            console.log($inputclick);
-            this.nextElementSibling.children[0].innerHTML = this.value;
-        });
+    function displayRangeValue($classe, $type){
+      
+      let $inputRange = document.querySelectorAll($classe);
+      if($inputRange){
+        for(var i=0;i<$inputRange.length;i++){
+            let $inputclick = $inputRange[i];
+            $inputclick.addEventListener('change', function(){
+                console.log(this.value);
+                this.nextElementSibling.children[0].innerHTML = this.value;
+                if($type == 'max'){
+                  //bloqué le min
+                }
+                else{ 
+                  //bloqué le max
+                }
+            });
+        }
+      }
     }
+
+    displayRangeValue('.content__aside-inputrange__maxrange input[type="range"]', 'max');
+    displayRangeValue('.content__aside-inputrange__minrange input[type="range"]', 'min');
+    
+
+   
 });
 
 
