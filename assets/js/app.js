168c29a3a09e94203d6fd527f5716c7964ea6a42
Gino Lee <ginoleeandriampaniry@gmail.com>
2023-04-28 11:59:16 +0300

    Filtre options

--- assets/js/app.js
+++ assets/js/app.js
@@ -63,8 +63,74 @@ document.addEventListener('DOMContentLoaded', function () {
 
   displayRangeValue('.content__aside-inputrange__maxrange input[type="range"]', 'max');
   displayRangeValue('.content__aside-inputrange__minrange input[type="range"]', 'min');
-    
 
+  //Filtrage
+  function filterInstant(){
+    console.log('filter instant');
+    let $items = document.getElementsByClassName('content__grid-layout__item');
+
+    let $inputRangeKmMin = document.querySelectorAll('.content__aside-inputrange.kilometer .slider-rangemin');
+    let $inputRangeKmMax = document.querySelectorAll('.content__aside-inputrange.kilometer .slider-rangemax');
+    
+    let $inputRangeYearMin = document.querySelectorAll('.content__aside-inputrange.year .slider-rangemin');
+    let $inputRangeYearMax = document.querySelectorAll('.content__aside-inputrange.year .slider-rangemax');
+
+    
+    let $inputRangePriceMin = document.querySelectorAll('.content__aside-inputrange.price .slider-rangemin');
+    let $inputRangePriceMax = document.querySelectorAll('.content__aside-inputrange.price .slider-rangemax');
+    
+    
+   
+    let $rangeMaxKmValue = parseInt($inputRangeKmMax[0].value);
+    let $rangeMinKmValue = parseInt($inputRangeKmMin[0].value);
+
+    let $rangeMaxYearValue = parseInt($inputRangeYearMax[0].value);
+    let $rangeMinYearValue = parseInt($inputRangeYearMin[0].value);
+
+    let $rangeMaxPriceValue = $inputRangePriceMax[0].value;
+    let $rangeMinPriceValue = $inputRangePriceMin[0].value;
+
+    
+    for(var i = 0; i<$items.length; i++){
+      let $kmFilter = false;
+      let $yearFilter = false;
+      let $priceFilter = false;
+
+      let $kmitem = parseInt($items[i].getAttribute('data-km'));
+      let $year = parseInt($items[i].getAttribute('data-year'));
+      let $price = $items[i].getAttribute('data-price');
+      
+      $price = parseInt($price.replace(' ', '').replace('.', '').replace('€', ''));
+      
+      
+      console.log($year,$rangeMinYearValue,$rangeMaxYearValue);
+      if($kmitem > $rangeMinKmValue && $kmitem < $rangeMaxKmValue){
+        $kmFilter = true;
+      }
+
+      if($year > $rangeMinYearValue && $year < $rangeMaxYearValue){
+        $yearFilter = true;
+      }
+
+      if($price > $rangeMinPriceValue && $year < $rangeMaxPriceValue){
+        $priceFilter = true;
+      }
+      console.log($kmFilter,$yearFilter,$priceFilter);
+      if($kmFilter && $yearFilter && $priceFilter){
+        if(!$items[i].classList.contains('active')){
+          $items[i].classList.add('active');
+        }
+      }
+      else{
+        if($items[i].classList.contains('active')){
+          $items[i].classList.remove('active');
+        }
+      }
+    }
+  }
+
+  document.getElementsByClassName('content__aside-form-btnsubmit')[0].addEventListener('click', filterInstant);
+  
    
 });
 
